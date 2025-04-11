a454bcae97ef27e059bd8fdf13601097439f5b41
luisanaarcias <luisanaarcias22@gmail.com>
2025-04-11 17:59:49 -0400

    add password user validation

--- controllers/login.js
+++ controllers/login.js
@@ -7,24 +7,32 @@ const jwt = require("jsonwebtoken");
 loginRouter.post("/", async (request, response) => {
 	const {email, password} = request.body;
 	const userExist = await User.findOne({email});
-
+	console.log(email);
+	console.log(password);
+	console.log(userExist.password);
+	
+	console.log('paso1');
 	if (!userExist) {
 		return response.status(400).json({error: "email o contraseña invalido"});
 	}
+	console.log('paso2');
 
 	const isCorrect = await bcrypt.compare(password, userExist.passwordHash);
 
 	if (!isCorrect) {
 		return response.status(400).json({error: "email o contraseña invalido"});
 	}
+	console.log('paso3');
 
 	const userForToken = {
 		id: userExist._id.toString(),
 	};
+	console.log('paso4');
 
 	const accesToken = jwt.sign(userForToken, process.env.ACCESS_TOKEN_SECRET, {
 		expiresIn: "1d",
 	});
+	console.log('paso5');
 
 	response.cookie("accesToken", accesToken, {
 		expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 1),
